776efbb21d753bb0aa205d6a7d446d0fc23ab6a8
LaurenE <Lauren.salitan@gmail.com>
2024-04-22 13:16:44 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,181 @@
+# # BEFORE:
+# from flask import Flask, request, render_template,  redirect, flash, session
+# from flask_debugtoolbar import DebugToolbarExtension
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+# # db=SQLAlchemy(app)
+# db = SQLAlchemy()
+# db.app = app
+# db.init_app(app)
+
+# app.config['SECRET_KEY'] = "chickenzarecool21837"
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
+
+# @app.route('/')
+# def home_page():
+#     """Shows home page"""
+#     return render_template('home.html')
+
+#####
+
+
+# # AFTER:
+from flask import Flask, request, render_template,  redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet #, get_pet_by_id
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] =  True
+app.config['SECRET_KEY'] = "chickenzarecool21837"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def list_pets():
+    """Shows list of all pets in db"""
+    pets = Pet.query.all()
+    return render_template('list.html', pets=pets)
+
+#video:
+@app.route('</int:pet_id>')
+def show_pet(pet_id):
+    """show details"""
+    pet = Pet.query.get(pet_id)
+    return f"<h1>(pet.name)</h1>"
+
+###Video later on:
+# @app.route('/', methods=["POST"])
+# def create_pet():
+#     name = request.form["name"]
+#     species = request.form["species"]
+#     hunger = request.form["hunger"]
+#     hunger = int(hunger) if hunger else None
+
+#     new_pet  = Pet(name=name, species=species, hunger=hunger)
+#     db.session.add(new_pet)
+#     db.session.commit()
+
+#     return redirect(f'/{new_pet.id}')
+
+# @app.route("/<int:pet_id>")
+# def show_pet(pet_id):
+#     """Show details about a single pet"""
+#     pet = Pet.query.get_or_404(pet_id)
+#     return render_template("details.html", pet=pet)
+
+# @app.route("/species/<species_id>")
+# def show_pets_by_species(species_id):
+#     pets = Pet.get_by_species(species_id)
+#     return render_template("species.html", pets=pets, species=species_id)
+###
+
+# # #attempt
+# # @app.route('</int:pet_id>')
+# # def show_pet(pet_id):
+# #     """show details"""
+# #     pet = db.session.get(Pet, id)
+# #     return f"<h1>(pet.name)</h1>"
+
+# # attempt 2
+# @app.route('</int:pet_name>')
+# def show_pet(pet_name):
+#     """show details"""
+#     pet = Pet.query.filter_by(name=pet_name).first
+#     return f"<h1>(pet.name)</h1>"
+
+
+
+
+
+##### attempt 3
+# from flask import Flask, request, render_template, redirect, flash, session
+# from flask_debugtoolbar import DebugToolbarExtension
+# from models import db, connect_db, Pet, get_pet_by_name
+
+# app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SECRET_KEY'] = "chickenzarecool21837"
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
+
+# connect_db(app)
+
+# @app.route('/')
+# def list_pets():
+#     """Shows list of all pets in db"""
+#     pets = Pet.query.all()
+#     return render_template('list.html', pets=pets)
+
+# @app.route('/pet/<name>')
+# def show_pet(name):
+#     """show details of a pet with given name"""
+#     pet = get_pet_by_name(name)
+#     if pet:
+#         return f"<h1>{pet.name}</h1>"
+#     else:
+#         return "<h1>Pet not found!</h1>"
+
+#####
+
+
+
+
+
+
+
+# @app.route('/<int:pet_id>')
+# def show_pet(pet_id):
+#     """show details of a pet with given ID"""
+#     pet = get_pet_by_id(pet_id)
+#     if pet:
+#         return f"<h1>{pet.name}</h1>"
+#     else:
+#         return "<h1>Pet not found!</h1>"
+
+# if __name__ == '__main__':
+#     with app.app_context():
+#         print("Database URL:", db.engine.url)
+#     app.run()
+
+
+# from flask import Flask, request, render_template, redirect, flash, session
+# from flask_debugtoolbar import DebugToolbarExtension
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+
+# # Set configuration before initializing the SQLAlchemy object
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+# app.config['SECRET_KEY'] = "chickenzarecool21837"
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+# # Initialize SQLAlchemy with the app
+# db = SQLAlchemy(app)
+
+# # Initialize DebugToolbarExtension with the app
+# debug = DebugToolbarExtension(app)
+
+# @app.route('/testdb')
+# def test_db():
+#     try:
+#         # Getting a connection and executing a query
+#         with db.engine.connect() as connection:
+#             result = connection.execute('SELECT 1')
+#             return str(result.fetchone())
+#     except Exception as e:
+#         return str(e)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,123 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+# attempted soluton:
+# def get_pet_by_id(id):
+#     """Get a pet by ID using a session."""
+#     return db.session.get(Pet, id)
+
+# models go below
+
+class Pet(db.Model):
+    """Pet."""
+    __tablename__ = "pets"
+
+    @classmethod
+    def get_by_species(cls, species):
+        return cls.query.filter_by(species=species).all()
+    
+    @classmethod
+    def get_all_hungry(cls):
+        return cls.query.filter(Pet.hunger >20).all()
+
+    # 
+    def __repr__(self):
+        p=self
+        return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}"
+    # def __repr__(self):
+    #     return f"<Pet id={self.id} name={self.name} species={self.species} hunger={self.hunger}>"
+
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    name = db.Column(db.String(50),
+                     nullable=False,
+                     unique=True)
+
+    species = db.Column(db.String(30), 
+                        nullable=True)
+
+    hunger = db.Column(db.Integer, 
+                       nullable=False, 
+                       default=20)
+    def greet(self):
+        return f"Hi, I am {self.name}, the {self.species}"
+    def feed(self, amt=20):
+        """update hunger based off amount"""
+        self.hunger -= amt
+        self.hunger = max(self.hunger, 0)
+
+
+
+
+##### attempt
+# from flask_sqlalchemy import SQLAlchemy
+
+# db = SQLAlchemy()
+
+# def connect_db(app):
+#     db.app = app
+#     db.init_app(app)
+
+# # Util function to get a pet by name
+# def get_pet_by_name(name):
+#     """Get a pet by name using a session."""
+#     return Pet.query.filter_by(name=name).first()
+
+# # models go below
+
+# class Pet(db.Model):
+#     """Pet."""
+#     __tablename__ = "pets"
+
+#     @classmethod
+#     def get_by_species(cls, species):
+#         return cls.query.filter_by(species=species).all()
+    
+#     @classmethod
+#     def get_all_hungry(cls):
+#         return cls.query.filter(Pet.hunger > 20).all()
+
+#     def __repr__(self):
+#         return f"<Pet id={self.id} name={self.name} species={self.species} hunger={self.hunger}>"
+
+#     id = db.Column(db.Integer,
+#                    primary_key=True,
+#                    autoincrement=True)
+#     name = db.Column(db.String(50),
+#                      nullable=False,
+#                      unique=True)
+#     species = db.Column(db.String(30), 
+#                         nullable=True)
+#     hunger = db.Column(db.Integer, 
+#                        nullable=False, 
+#                        default=20)
+
+#     def greet(self):
+#         return f"Hi, I am {self.name}, the {self.species}"
+
+#     def feed(self, amt=20):
+#         """Update hunger based off amount fed."""
+#         self.hunger -= amt
+#         self.hunger = max(self.hunger, 0)
+#####
+    
+# kyle = Pet(name="kyle the chicken", species="Chicken")
+# kyle.name="Kyle The Rooster"
+
+# kyle.getOffspring()
+
+# CREATE TABLE pets {
+#     id abjhe
+#     name hjsdb
+#     pecies jhdb
+# }
+
+
